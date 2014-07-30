a96ae74cb6f4cc18825ed5c122aa76c8876e15cd
krutika-ostwal <krutika.ostwal@cumminscollege.in>
2014-07-30 14:30:48 +0530

    Update calc.cpp
    Square Method added.

--- calc.cpp
+++ calc.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 void main()
 {
-cout<<"Hello";
+cout<<"Welcome to GitHub";
+cout<<"Square";
+}
+
+double square(double in_num)
+{
+  double out_num;
+  out_num=in_num;
+  return out_num;
 }
